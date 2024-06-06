64db9dab6eb24c7d15918d47db788950d5c14cf7
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-06-07 01:07:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function insert_Row() {
-    //Write your code here
+  var table = document.getElementById("sampleTable");
   
-  
+	var row =table.insertRow(0);
+	row.insertCell(0).innerHTML="New cell1";
+	row.insertCell(1).innerHTML="New cell2";
+	
 }
